6ca7fbe5b9b2752d7e6f6d081f06e73f368c6530
Harshchafle <chafleharsh@gmail.com>
2025-01-01 18:27:15 +0530

    .concat

--- Arrays.js
+++ Arrays.js
@@ -1,11 +1,16 @@
-nums = [1,2,3,42,54,6,7,8,9]
-nums.push(100)
-console.log(nums)
-nums.pop()
-console.log(nums) 
-nums.unshift(20) // -> insert at index0
-console.log(nums)
-console.log(nums.includes(200))
-console.log(nums.indexOf(30))
-console.log(typeof(nums.join()))
+// nums = [1,2,3,42,54,6,7,8,9]
+// nums.push(100)
+// console.log(nums)
+// nums.pop()
+// console.log(nums) 
+// nums.unshift(20) // -> insert at index0
+// console.log(nums)
+// console.log(nums.includes(200))
+// console.log(nums.indexOf(30))
+// console.log(typeof(nums.join()))
 // what is difference between slice() and splice() 
+
+const marvel = ["a","b","c","d"]
+const dc = [1,2,3,4]
+console.log((marvel+dc),"\n",marvel.concat(dc),"\n",(marvel.push(dc)))
+console.log()
